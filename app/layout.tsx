99a0847e851badabddb21936f7ccc5911876af5a
Tushar <tushar158gupta@gmail.com>
2025-04-06 18:15:00 +0530

    inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Mona_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const MonaSans = Mona_Sans({
+  variable: "--font-Mona-sans",
   subsets: ["latin"],
 });
 
@@ -23,12 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className="dark">
+      <body className={`${MonaSans.variable}  antialiased`}>{children}</body>
     </html>
   );
 }
